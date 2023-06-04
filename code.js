f88f5f2ba0f98332d5c306aef9712c186ca8e6a0
Eddy Ding <edwarding04@gmail.com>
2023-06-04 15:14:03 -0400

    Fix layout issues and start on grid generation

--- code.js
+++ code.js
@@ -1,10 +1,13 @@
 const colorSlider = document.getElementById("color");;
 const brightnessSlider = document.getElementById("brightness");
 const colorDisplay = document.getElementById("selected-color");
+const itemsContainer = document.getElementById("boxes-container");
 let hue = 0;
 let brightness = 50;
 let color = 'hsl(' + hue + ', 100%, ' + brightness +'%)';
+let dimensions = 10;
 
+updateBoard(dimensions);
 brightnessSlider.style.backgroundColor ='hsl(' + 0 + ', 0%, 70%)';
 colorDisplay.style.backgroundColor = 'hsl(' + hue + ', 100%, ' + brightness +'%)';
 
@@ -22,3 +25,13 @@ brightnessSlider.addEventListener("input", () =>{
     colorDisplay.style.backgroundColor = color;
 });
 
+function updateBoard(dim){
+    const row = document.createElement('div');
+    for(let i = 0; i < dim; i++){
+        const item = document.createElement('div');
+        item.classList.add("tile");
+        row.appendChild(item);
+    }
+    row.classList.add("row");
+    itemsContainer.appendChild(row);
+}
